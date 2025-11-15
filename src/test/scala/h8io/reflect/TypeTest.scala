9c3c9bd49454549e0b4b0c63bc919e493458f65b
Eshu <tjano.xibalba@gmail.com>
2025-11-15 13:54:09 +0900

    accepts test

--- src/test/scala/h8io/reflect/TypeTest.scala
+++ src/test/scala/h8io/reflect/TypeTest.scala
@@ -151,4 +151,79 @@ class TypeTest extends AnyFlatSpec with Matchers {
     implicitly[Type[String => Unit]] =:= implicitly[Type[PartialFunction[Nothing, Unit]]] shouldBe false
     implicitly[Type[CharSequence => Unit]] =:= implicitly[Type[PartialFunction[String, Unit]]] shouldBe false
   }
+
+  "accepts" should "work for a primitive type" in {
+    implicitly[Type[Int]].accepts(implicitly[Type[Int]]) shouldBe true
+
+    implicitly[Type[Int]].accepts(implicitly[Type[Any]]) shouldBe false
+    implicitly[Type[Int]].accepts(implicitly[Type[AnyVal]]) shouldBe false
+    implicitly[Type[Int]].accepts(implicitly[Type[AnyRef]]) shouldBe false
+    implicitly[Type[Int]].accepts(implicitly[Type[Integer]]) shouldBe false
+    implicitly[Type[Integer]].accepts(implicitly[Type[Int]]) shouldBe false
+  }
+
+  it should "work for a plain reference type" in {
+    implicitly[Type[String]].accepts(implicitly[Type[String]]) shouldBe true
+
+    implicitly[Type[String]].accepts(implicitly[Type[AnyVal]]) shouldBe false
+    implicitly[Type[String]].accepts(implicitly[Type[AnyRef]]) shouldBe false
+    implicitly[Type[String]].accepts(implicitly[Type[CharSequence]]) shouldBe false
+    implicitly[Type[String]].accepts(implicitly[Type[Nothing]]) shouldBe false
+    implicitly[Type[CharSequence]].accepts(implicitly[Type[String]]) shouldBe false
+  }
+
+  it should "work for a reference type with invariant type parameters" in {
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.Collection[CharSequence]]]) shouldBe false
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.Collection[String]]]) shouldBe false
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.Collection[Nothing]]]) shouldBe false
+    implicitly[Type[ju.List[CharSequence]]].accepts(implicitly[Type[ju.Collection[String]]]) shouldBe false
+
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.List[CharSequence]]]) shouldBe false
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.List[String]]]) shouldBe true
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.List[Nothing]]]) shouldBe false
+    implicitly[Type[ju.List[CharSequence]]].accepts(implicitly[Type[ju.List[String]]]) shouldBe false
+
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.ArrayList[CharSequence]]]) shouldBe false
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.ArrayList[String]]]) shouldBe false
+    implicitly[Type[ju.List[String]]].accepts(implicitly[Type[ju.ArrayList[Nothing]]]) shouldBe false
+    implicitly[Type[ju.List[CharSequence]]].accepts(implicitly[Type[ju.ArrayList[String]]]) shouldBe false
+  }
+
+  it should "work for a reference type with covariant type parameters" in {
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Iterable[CharSequence]]]) shouldBe false
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Iterable[String]]]) shouldBe false
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Iterable[Nothing]]]) shouldBe false
+    implicitly[Type[Seq[CharSequence]]].accepts(implicitly[Type[Iterable[String]]]) shouldBe false
+
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Seq[CharSequence]]]) shouldBe false
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Seq[String]]]) shouldBe true
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[Seq[Nothing]]]) shouldBe false
+    implicitly[Type[Seq[CharSequence]]].accepts(implicitly[Type[Seq[String]]]) shouldBe false
+
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[List[CharSequence]]]) shouldBe false
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[List[String]]]) shouldBe false
+    implicitly[Type[Seq[String]]].accepts(implicitly[Type[List[Nothing]]]) shouldBe false
+    implicitly[Type[Seq[CharSequence]]].accepts(implicitly[Type[List[String]]]) shouldBe false
+  }
+
+  it should "work for a reference type with contravariant type parameters" in {
+    implicitly[Type[PartialFunction[String, Unit]]].accepts(implicitly[Type[CharSequence => Unit]]) shouldBe false
+    implicitly[Type[PartialFunction[String, Unit]]].accepts(implicitly[Type[String => Unit]]) shouldBe false
+    implicitly[Type[PartialFunction[String, Unit]]].accepts(implicitly[Type[Nothing => Unit]]) shouldBe false
+    implicitly[Type[PartialFunction[CharSequence, Unit]]].accepts(implicitly[Type[String => Unit]]) shouldBe false
+
+    implicitly[Type[PartialFunction[String, Unit]]]
+      .accepts(implicitly[Type[PartialFunction[CharSequence, Unit]]]) shouldBe false
+    implicitly[Type[PartialFunction[String, Unit]]]
+      .accepts(implicitly[Type[PartialFunction[String, Unit]]]) shouldBe true
+    implicitly[Type[PartialFunction[String, Unit]]]
+      .accepts(implicitly[Type[PartialFunction[Nothing, Unit]]]) shouldBe false
+    implicitly[Type[PartialFunction[CharSequence, Unit]]]
+      .accepts(implicitly[Type[PartialFunction[String, Unit]]]) shouldBe false
+
+    implicitly[Type[String => Unit]].accepts(implicitly[Type[PartialFunction[CharSequence, Unit]]]) shouldBe false
+    implicitly[Type[String => Unit]].accepts(implicitly[Type[PartialFunction[String, Unit]]]) shouldBe false
+    implicitly[Type[String => Unit]].accepts(implicitly[Type[PartialFunction[Nothing, Unit]]]) shouldBe false
+    implicitly[Type[CharSequence => Unit]].accepts(implicitly[Type[PartialFunction[String, Unit]]]) shouldBe false
+  }
 }
